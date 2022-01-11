d8916559d704f633c107d3754d7fc3558e5417b9
Andres Blandon <andblandon@hotmail.com>
2022-01-11 04:18:37 +0000

    using puppet to create HTTP

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,21 @@
+# automate the task of creating a custom HTTP header response, but with Puppet
+exec { 'update':
+    command => "/usr/bin/apt-get -y update"
+}
+
+package { 'nginx':
+    ensure   => 'installed',
+    provider => 'apt',
+    require => Exec['update']
+}
+
+exec { 'Create custom HTTP header':
+    command => "/usr/bin/env sed -i '/^\tserver_name.*/a \\\tadd_header X-Served-By ${hostname};\n' /etc/nginx/sites-available/default"
+    require => Package['nginx']
+}
+
+service { 'restart':
+    ensure  => running,
+    require => Exec['Create custom HTTP header'],
+    restart => 'sudo service nginx restart',
+}
