42113db43fbd5bac5ce1e4c90691ee7e3e363754
Andres Blandon <andblandon@hotmail.com>
2022-02-17 00:25:06 +0000

    Update req

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
+# Using Strace to fix a problem
 exec { 'fix-wordpress':
-     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-     provider => shell,
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  provider => shell,
 }
